d52df619ceb917d7c1c05ddffce8ce77c8253e32
Oliver Zilken <ozilken@gmail.com>
2012-10-10 21:32:46 +0200

    added first rectangle recognition stuff

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-g++ `pkg-config opencv --cflags` $1  -o stromding_app `pkg-config opencv --libs`
+if [ -f stromding_app ]; then rm stromding_app; fi
+g++ `pkg-config opencv --cflags` stromding.cpp  -o stromding_app `pkg-config opencv --libs` && echo Finished building stromding_app
--- stromding.cpp
+++ stromding.cpp
@@ -1,94 +1,181 @@
-/********************************************************************************
-*
-*
-*  This program is demonstration for ellipse fitting. Program finds
-*  contours and approximate it by ellipses.
-*
-*  Trackbar specify threshold parametr.
-*
-*  White lines is contours. Red lines is fitting ellipses.
-*
-*
-*  Autor:  Denis Burenkov.
-*
-*
-*
-********************************************************************************/
+// The "Square Detector" program.
+// It loads several images sequentially and tries to find squares in
+// each image
+
+#include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+
 #include <iostream>
+#include <math.h>
+#include <string.h>
+
 using namespace cv;
 using namespace std;
 
 void help()
 {
 	cout <<
-			"\nThis program is demonstration for ellipse fitting. The program finds\n"
-			"contours and approximate it by ellipses.\n"
-			"Call:\n"
-			"./fitellipse [image_name -- Default stuff.jpg]\n" << endl;
+	"\nA program using pyramid scaling, Canny, contours, contour simpification and\n"
+	"memory storage (it's got it all folks) to find\n"
+	"squares in a list of images pic1-6.png\n"
+	"Returns sequence of squares detected on the image.\n"
+	"the sequence is stored in the specified memory storage\n"
+	"Call:\n"
+	"./squares\n"
+    "Using OpenCV version %s\n" << CV_VERSION << "\n" << endl;
 }
 
-int sliderPos = 70;
 
-Mat image;
+int thresh = 3, N = 1;
+const char* wndname = "Square Detection Demo";
 
-void processImage(int, void*);
-
-int main( int argc, char** argv )
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+double angle( Point pt1, Point pt2, Point pt0 )
 {
-    const char* filename = argc == 2 ? argv[1] : (char*)"stuff.jpg";
-    image = imread(filename, 0);
-    if( image.empty() )
-    {
-        cout << "Couldn't open image " << filename << "\nUsage: fitellipse <image_name>\n";
-        return 0;
-    }
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
 
-    imshow("source", image);
-    namedWindow("result", 1);
+// returns sequence of squares detected on the image.
+// the sequence is stored in the specified memory storage
+void findSquares( const Mat& image, vector<vector<Point> >& squares )
+{
+    squares.clear();
+    int numsquares = 0;
     
-    // Create toolbars. HighGUI use.
-    createTrackbar( "threshold", "result", &sliderPos, 255, processImage );
-    processImage(0, 0);
-
-    // Wait for a key stroke; the same function arranges events processing
-    waitKey();
-    return 0;
-}
-
-// Define trackbar callback functon. This function find contours,
-// draw it and approximate it by ellipses.
-void processImage(int /*h*/, void*)
-{
+    Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+    
+    // down-scale and upscale the image to filter out the noise
+    pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
+    pyrUp(pyr, timg, image.size());
     vector<vector<Point> > contours;
-    Mat bimage = image >= sliderPos;
     
-    findContours(bimage, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-
-    Mat cimage = Mat::zeros(bimage.size(), CV_8UC3);
-
-    for(size_t i = 0; i < contours.size(); i++)
+    // find squares in every color plane of the image
+    for( int c = 0; c < 3; c++ )
     {
-        size_t count = contours[i].size();
-        if( count < 6 )
-            continue;
+        int ch[] = {c, 0};
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
         
-        Mat pointsf;
-        Mat(contours[i]).convertTo(pointsf, CV_32F);
-        RotatedRect box = fitEllipse(pointsf);
-        
-        if( MAX(box.size.width, box.size.height) > MIN(box.size.width, box.size.height)*30 )
-            continue;
-        drawContours(cimage, contours, (int)i, Scalar::all(255), 1, 8);
+        // try several threshold levels
+        for( int l = 0; l < N; l++ )
+        {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if( l == 0 )
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                Canny(gray0, gray, 0, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1,-1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l+1)*255/N;
+            }
 
-        ellipse(cimage, box, Scalar(0,0,255), 1, CV_AA);
-        ellipse(cimage, box.center, box.size*0.5f, box.angle, 0, 360, Scalar(0,255,255), 1, CV_AA);
-        Point2f vtx[4];
-        box.points(vtx);
-        for( int j = 0; j < 4; j++ )
-            line(cimage, vtx[j], vtx[(j+1)%4], Scalar(0,255,0), 1, CV_AA);
+            // find contours and store them all as a list
+            findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+            vector<Point> approx;
+            
+            // test each contour
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+                
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if( approx.size() == 4 &&
+                    fabs(contourArea(Mat(approx))) > 1000 &&
+                    isContourConvex(Mat(approx)) )
+                {
+                    double maxCosine = 0;
+
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+	
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if( maxCosine < 0.3 ){
+                        squares.push_back(approx);
+			numsquares++;
+			Point ul=approx[0];
+			Point ur=approx[0];
+			Point ll=approx[0];
+			Point lr=approx[0];
+			cout << "\n Found  square " << numsquares << ":";
+			for( int i = 0; i < 4; i++ ){
+				Point p = approx[i];
+				if(p.x <= ul.x && p.y >= ul.y) ul = p;
+				if(p.x >= ur.x && p.y >= ur.y) ur=p;
+				if(p.x <= ll.x && p.y <= ll.y) ll=p;
+				if(p.x >= lr.x && p.y <= lr.y) lr=p;
+			//	cout << "(" << approx[i].x<<","<< approx[i].y<< "), ";	
+			}
+			cout << "ul("<<ul.x<<","<<ul.y<<"),"<<"ur("<<ur.x<<","<<ur.y<<"),"<<"ll("<<ll.x<<","<<ll.y<<"),"<<"lr("<<lr.x<<","<<lr.y<<"). ";
+			cout << endl;
+				
+		    }
+                }
+            }
+        }
+    }
+}
+
+
+// the function draws all the squares in the image
+void drawSquares( Mat& image, const vector<vector<Point> >& squares )
+{
+    for( size_t i = 0; i < squares.size(); i++ )
+    {
+        const Point* p = &squares[i][0];
+        int n = (int)squares[i].size();
+        polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, CV_AA);
     }
 
-    imshow("result", cimage);
+    imshow(wndname, image);
+}
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    static const char* filename =  "img/sample1.jpg";
+    help();
+    namedWindow( wndname, 1 );
+    vector<vector<Point> > squares;
+    
+        Mat image = imread(filename, 1);
+        if( image.empty() )
+        {
+            cout << "Couldn't load " << filename << endl;
+            return 1;
+        }
+        
+        findSquares(image, squares);
+        drawSquares(image, squares);
+
+        int c = waitKey();
+
+    return 0;
 }
